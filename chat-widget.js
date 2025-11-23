e85e7e47676a6184352d95a03c9201304753241b
Cyber <cybr1998@gmail.com>
2025-11-23 08:10:51 +0530

    replace botscript script

--- chat-widget.js
+++ chat-widget.js
@@ -31,17 +31,22 @@
   // Initialize Botsonic widget
   (function (w, d, s, o, f, js, fjs) {
     w["botsonic_widget"] = o;
-    w[o] = w[o] || function () {
-      (w[o].q = w[o].q || []).push(arguments);
-    };
-    js = d.createElement(s);
-    fjs = d.getElementsByTagName(s)[0];
+    w[o] =
+      w[o] ||
+      function () {
+        (w[o].q = w[o].q || []).push(arguments);
+      };
+    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
     js.id = o;
     js.src = f;
     js.async = 1;
     fjs.parentNode.insertBefore(js, fjs);
   })(window, document, "script", "Botsonic", "https://widget.botsonic.com/CDN/botsonic.min.js");
-
+  Botsonic("init", {
+    serviceBaseUrl: "https://api-bot.writesonic.com",
+    token: "3255c3ee-fb9a-429c-93b7-c474ef0a82b1",
+  });
+  
   // Wait for Botsonic script to load, then initialize
   let initAttempts = 0;
   const maxAttempts = 50;
